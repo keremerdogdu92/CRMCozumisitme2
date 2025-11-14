19b64b86c7226ae360a6ec5f8c1ef955bdb411b8
keremerdogdu92 <keremerdogdu92@gmail.com>
2025-11-14 04:09:04 +0300

    Remove BrowserRouter from main.tsx
    Removed BrowserRouter wrapper around App component.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
+// src/main.tsx
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 import './index.css';
@@ -10,9 +11,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </QueryClientProvider>
   </React.StrictMode>
 );
