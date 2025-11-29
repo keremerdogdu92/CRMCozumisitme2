ff1b73afd3d89d5d57f408366195c4a12fcc1c4c
keremerdogdu92 <keremerdogdu92@gmail.com>
2025-11-29 12:17:20 +0300

    Delete src/features/patients/api.ts

--- src/features/patients/api.ts
+++
@@ -1,6 +0,0 @@
-// src/features/patients/api.ts
-// Public barrel for Patients API helpers; re-exports domain modules.
-
-export * from './api.core';
-export * from './api.patients';
-export * from './api.payments';
