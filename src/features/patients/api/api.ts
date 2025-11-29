719ad89588e2221c7ff6aeca3241d6fa48a47ef0
keremerdogdu92 <keremerdogdu92@gmail.com>
2025-11-29 12:16:53 +0300

    Create api.ts

---
+++ src/features/patients/api/api.ts
@@ -0,0 +1,6 @@
+// src/features/patients/api.ts
+// Public barrel for Patients API helpers; re-exports domain modules.
+
+export * from './api.core';
+export * from './api.patients';
+export * from './api.payments';
